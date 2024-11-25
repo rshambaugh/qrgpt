e8f861cf75d9c45da92b832a4e8f80bf5e3a5b7b
Rob Shambaugh <rob.shambaugh@gmail.com>
2024-11-24 23:12:34 -0500

    Added ReactDOM info

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -783,3 +783,8 @@ const handleUpdate = async (e) => {
 }
 
 export default App;
+
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
