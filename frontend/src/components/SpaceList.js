edbcf397f628fd0d88b6ca7fdae28cd7b76f6fbd
Rob Shambaugh <rob.shambaugh@gmail.com>
2024-12-06 05:36:07 -0500

    Trying to get drag and drop working.wq

--- frontend/src/components/SpaceList.js
+++ frontend/src/components/SpaceList.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Space from './Space';
 
-const SpaceList = ({ spaces, items, onDrop, onDelete }) => {
+const SpaceList = ({ spaces, items, onDropItem, onDropSpace, onDelete }) => {
     return (
-        <div className="space-list">
+        <div>
             {spaces.map((space) => (
                 <Space
                     key={space.id}
                     space={space}
-                    items={items}
-                    onDrop={onDrop}
+                    items={items.filter((item) => item.space_id === space.id)}
+                    spaces={spaces.filter((childSpace) => childSpace.parent_id === space.id)}
+                    onDropItem={onDropItem}
+                    onDropSpace={onDropSpace}
                     onDelete={onDelete}
                 />
             ))}
