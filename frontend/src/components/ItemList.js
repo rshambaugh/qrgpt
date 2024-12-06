eb6ea0c7b45740ed9ee3a0b152ecdba2fdd47e7a
Rob Shambaugh <rob.shambaugh@gmail.com>
2024-12-06 06:24:17 -0500

    Adjusting drag and drop

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import Item from './Item';
+import React from "react";
+import { useDrag } from "react-dnd";
 
-const ItemList = ({ items, onDelete }) => {
-    return (
-        <div className="item-list">
-            {items.map((item) => (
-                <Item key={item.id} item={item} onDelete={onDelete} />
-            ))}
-        </div>
-    );
+const Item = ({ item, onDelete }) => {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: "ITEM",
+    item: { id: item.id, type: "item" },
+    collect: (monitor) => ({ isDragging: monitor.isDragging() }),
+  }));
+
+  return (
+    <div ref={drag} className={`item ${isDragging ? "item-dragging" : ""}`}>
+      {item.name}
+      <button className="delete-button" onClick={() => onDelete(item.id)}>
+        Delete
+      </button>
+    </div>
+  );
 };
+
+const ItemList = ({ items, onDrop }) => (
+  <div className="item-list">
+    {items.map((item) => (
+      <Item key={item.id} item={item} onDelete={(id) => onDrop(id, null)} />
+    ))}
+  </div>
+);
 
 export default ItemList;
--- frontend/src/components/SpaceList.js
+++ frontend/src/components/SpaceList.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import Space from './Space';
+import React from "react";
+import Space from "./Space";
 
-const SpaceList = ({ spaces, items, onDropItem, onDropSpace, onDelete }) => {
-    return (
-        <div>
-            {spaces.map((space) => (
-                <Space
-                    key={space.id}
-                    space={space}
-                    items={items.filter((item) => item.space_id === space.id)}
-                    spaces={spaces.filter((childSpace) => childSpace.parent_id === space.id)}
-                    onDropItem={onDropItem}
-                    onDropSpace={onDropSpace}
-                    onDelete={onDelete}
-                />
-            ))}
-        </div>
-    );
+const SpaceList = ({ spaces, items, onDrop }) => {
+  const renderSpaces = (parentId) =>
+    spaces
+      .filter((space) => space.parent_id === parentId)
+      .map((space) => (
+        <Space
+          key={space.id}
+          space={space}
+          items={items.filter((item) => item.space_id === space.id)}
+          onDrop={onDrop}
+        >
+          {renderSpaces(space.id)}
+        </Space>
+      ));
+
+  return <div className="space-list">{renderSpaces(null)}</div>;
 };
 
 export default SpaceList;
